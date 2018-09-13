c648021c296148ceb18786e09b1651ac8149a1bd
MartinSmel <43166833+MartinSmel@users.noreply.github.com>
2018-09-13 15:23:49 +0200

    wiki_graph

---
+++ R/wiki_graph.R
@@ -0,0 +1,20 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(wiki_graph)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://en.wikipedia.org/wiki/Graph}{QTL Archive}
+#'
+#' 
+"wiki_graph"
